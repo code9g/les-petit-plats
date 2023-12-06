76988de38916c4f96cc9995d05cd280aa639275e
Pierre HENRY <pierre.henry@code99.fr>
2023-12-06 10:16:25 +0100

    mise en fonction createLiveSearch

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -8,49 +8,163 @@ function replaceDiacritic(str) {
   return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
 }
 
+const openDropdown = (element) => {
+  element
+    .closest(".dropdown")
+    .querySelector(".dropdown-content")
+    .classList.remove("hidden");
+};
+
+const closeDropdown = (element) => {
+  return false;
+  element
+    .closest(".dropdown")
+    .querySelector(".dropdown-content")
+    .classList.add("hidden");
+};
+
+const toggleDropdown = (element) => {
+  element
+    .closest(".dropdown")
+    .querySelector(".dropdown-content")
+    .classList.toggle("hidden");
+};
+
+const selectEvent = (e) => {
+  e.stopPropagation();
+  e.currentTarget.setAttribute("data-selected", "");
+  closeDropdown(e.currentTarget);
+};
+
+const unselectedEvent = (e) => {
+  e.stopPropagation();
+  const target = e.currentTarget.closest("li");
+  target.removeAttribute("data-selected");
+  closeDropdown(target);
+};
+
+const toggleDropDownEvent = (e) => {
+  e.preventDefault();
+  toggleDropdown(e.currentTarget);
+};
+
+function createLiveSearch(
+  name,
+  title,
+  list,
+  onSelect,
+  onUnselect,
+  onOpen,
+  onClose
+) {
+  const result = document.createElement("div");
+  result.id = `${name}-filter`;
+  result.className = "dropdown";
+
+  const button = document.createElement("button");
+  button.type = "button";
+  button.className = "dropdown-button";
+  button.textContent = title;
+
+  const content = document.createElement("div");
+  content.className = "dropdown-content hidden";
+
+  const search = document.createElement("div");
+  search.className = "dropdown-search";
+
+  const input = document.createElement("input");
+  input.type = "text";
+  input.id = `${name}-search`;
+  input.name = input.id;
+  input.className = "search";
+  input.addEventListener("input", (e) => {
+    e.preventDefault();
+    const items = ul.childNodes;
+    const text = replaceDiacritic(e.currentTarget.value.trim());
+    if (text === "") {
+      for (const li of items) {
+        li.removeAttribute("data-hidden");
+      }
+    } else {
+      const re = new RegExp(escapeRegex(text), "gi");
+      for (const li of items) {
+        const ref = li.dataset.search;
+        if (re.exec(ref)) {
+          li.removeAttribute("data-hidden");
+        } else {
+          li.setAttribute("data-hidden", "");
+        }
+      }
+    }
+  });
+
+  search.appendChild(input);
+
+  content.appendChild(search);
+
+  const ul = document.createElement("ul");
+  ul.className = "list";
+
+  for (let i = 0; i < list.length; i++) {
+    const item = list[i];
+
+    const li = document.createElement("li");
+
+    li.dataset.search = replaceDiacritic(item.trim()).toLowerCase();
+    li.dataset.key = i;
+
+    const text = document.createElement("span");
+    text.textContent = item;
+
+    li.appendChild(text);
+
+    const btnUnselect = document.createElement("button");
+    btnUnselect.className = "close";
+    btnUnselect.type = "button";
+    btnUnselect.ariaLabel = "Déselectionner";
+    btnUnselect.ariaHidden = "true";
+    li.appendChild(btnUnselect);
+
+    li.addEventListener("click", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      li.setAttribute("data-selected", "");
+    });
+    btnUnselect.addEventListener("click", (e) => {
+      e.preventDefault();
+      e.stopPropagation();
+      li.removeAttribute("data-selected");
+    });
+
+    ul.appendChild(li);
+  }
+
+  content.appendChild(ul);
+  result.appendChild(button);
+  result.appendChild(content);
+  button.addEventListener("click", (e) => {
+    e.preventDefault();
+    e.stopPropagation();
+    content.classList.toggle("hidden");
+  });
+  return result;
+}
+
 class App {
   constructor() {}
 
+  liveSearch(target, list) {}
+
   async run() {
-    const i1 = document.querySelector("#ingredients-filter");
-    const btn1 = i1.querySelector("button");
+    const filter1 = createLiveSearch("ingredients", "Ingrédients", ingredients);
+    const filter2 = createLiveSearch("appliances", "Appareils", appliances);
+    const filter3 = createLiveSearch("ustensils", "Ustensiles", ustensils);
 
-    btn1.addEventListener("click", (e) => {
-      e.currentTarget
-        .closest(".live-search")
-        .querySelector(".content")
-        .classList.toggle("hidden");
-    });
+    const filters = document.querySelector(".filters .container");
 
-    const ul1 = i1.querySelector(".list");
-    ul1.innerHTML = "";
-    for (const ingredient of ingredients) {
-      const li = document.createElement("li");
-      li.textContent = ingredient;
-      ul1.appendChild(li);
-    }
-
-    const items1 = i1.querySelectorAll("li");
-
-    i1.querySelector(".content input").addEventListener("input", (e) => {
-      const text = replaceDiacritic(e.currentTarget.value.trim());
-      if (text === "") {
-        items1.forEach((element) => {
-          element.classList.remove("hidden");
-        });
-      } else {
-        const re = new RegExp(escapeRegex(text), "gi");
-        items1.forEach((element) => {
-          const ref = replaceDiacritic(element.innerText);
-          console.log(ref);
-          if (re.exec(ref)) {
-            element.classList.remove("hidden");
-          } else {
-            element.classList.add("hidden");
-          }
-        });
-      }
-    });
+    filters.append(filter1);
+    filters.append(filter2);
+    filters.append(filter3);
 
     const wrapper = document.querySelector(".recipes .cards");
     wrapper.innerHTML = "";
