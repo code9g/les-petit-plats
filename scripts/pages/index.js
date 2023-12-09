4759d2aa7c9df00107095f8fad20a3566ea8c747
Pierre HENRY <pierre.henry@code99.fr>
2023-12-09 01:02:02 +0100

    Refactoring recipeCard

--- scripts/pages/index.js
+++ scripts/pages/index.js
@@ -1,4 +1,5 @@
 import { recipes, ingredients, ustensils, appliances } from "../recipes.js";
+import { recipeCardTemplate } from "../templates/recipeCard.js";
 
 const inputEvent = new CustomEvent("input");
 
@@ -189,68 +190,19 @@ class App {
     });
 
     const wrapper = document.querySelector(".recipes .cards");
-    wrapper.innerHTML = "";
     for (const recipe of recipes) {
-      const card = document.createElement("article");
-      card.className = "card";
-      card.dataset.id = recipe.id;
-
-      const img = document.createElement("img");
-      img.src = `assets/photos/${recipe.image}`;
-      img.className = "card-img-top";
-      img.alt = "Photo de la recette";
-
-      const badge = document.createElement("span");
-      badge.className = "badge";
-      badge.textContent = `${recipe.time} mn`;
-
-      const body = document.createElement("div");
-      body.className = "card-body";
-
-      const h2 = document.createElement("h2");
-      h2.className = "title";
-      h2.textContent = recipe.name;
-
-      const h3a = document.createElement("h3");
-      h3a.textContent = "Recette";
-      const pa = document.createElement("p");
-      pa.className = "description";
-      pa.textContent = recipe.description;
-
-      const h3b = document.createElement("h3");
-      h3b.textContent = "Ingrédients";
-
-      const divb = document.createElement("div");
-      divb.className = "ingredients";
-      for (const ingredient of recipe.ingredients) {
-        const divi = document.createElement("div");
-        divi.className = "ingredient";
-        const h4 = document.createElement("h4");
-        h4.textContent = ingredient.ingredient;
-        const p = document.createElement("p");
-        p.className = "quantity";
-        if (ingredient.unit) {
-          p.textContent = `${ingredient.quantity} ${ingredient.unit}`;
-        } else {
-          p.textContent = `${ingredient.quantity}`;
-        }
-        divi.appendChild(h4);
-        divi.appendChild(p);
-        divb.appendChild(divi);
-      }
-
-      body.appendChild(h2);
-      body.appendChild(h3a);
-      body.appendChild(pa);
-      body.appendChild(h3b);
-      body.appendChild(divb);
-
-      card.appendChild(img);
-      card.appendChild(badge);
-      card.appendChild(body);
-
-      wrapper.appendChild(card);
+      wrapper.appendChild(recipeCardTemplate(recipe));
     }
+  }
+
+  updateSearch() {
+    const search = "";
+    // Récuperer le texte de la recherche principale
+    // Récuperer les tags choisies
+    // Filtrer les recettes en fonction du texte et des tags
+    // Regénérer les listes des tags (ingrédients, appareils et ustensils)
+    // Mettre à jour la liste des tags disponibles
+    // Mettre à jour la liste des recettes
   }
 }
 
---
+++ scripts/templates/recipeCard.js
@@ -0,0 +1,61 @@
+export function recipeCardTemplate(recipe) {
+  const card = document.createElement("article");
+  card.className = "card";
+  card.dataset.id = recipe.id;
+
+  const img = document.createElement("img");
+  img.src = `assets/photos/${recipe.image}`;
+  img.className = "card-img-top";
+  img.alt = "Photo de la recette";
+
+  const badge = document.createElement("span");
+  badge.className = "badge";
+  badge.textContent = `${recipe.time} mn`;
+
+  const body = document.createElement("div");
+  body.className = "card-body";
+
+  const h2 = document.createElement("h2");
+  h2.className = "title";
+  h2.textContent = recipe.name;
+
+  const h3a = document.createElement("h3");
+  h3a.textContent = "Recette";
+  const pa = document.createElement("p");
+  pa.className = "description";
+  pa.textContent = recipe.description;
+
+  const h3b = document.createElement("h3");
+  h3b.textContent = "Ingrédients";
+
+  const divb = document.createElement("div");
+  divb.className = "ingredients";
+  for (const ingredient of recipe.ingredients) {
+    const divi = document.createElement("div");
+    divi.className = "ingredient";
+    const h4 = document.createElement("h4");
+    h4.textContent = ingredient.ingredient;
+    const p = document.createElement("p");
+    p.className = "quantity";
+    if (ingredient.unit) {
+      p.textContent = `${ingredient.quantity} ${ingredient.unit}`;
+    } else {
+      p.textContent = `${ingredient.quantity}`;
+    }
+    divi.appendChild(h4);
+    divi.appendChild(p);
+    divb.appendChild(divi);
+  }
+
+  body.appendChild(h2);
+  body.appendChild(h3a);
+  body.appendChild(pa);
+  body.appendChild(h3b);
+  body.appendChild(divb);
+
+  card.appendChild(img);
+  card.appendChild(badge);
+  card.appendChild(body);
+
+  return card;
+}
